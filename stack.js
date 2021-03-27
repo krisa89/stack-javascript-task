5f080eabc622029ed90aecdfd2774cec102a031b
krisa89 <krisa89@gmail.com>
2021-03-27 21:48:29 +0100

    Finish stack implementation

--- stack.js
+++ stack.js
@@ -1,1 +1,34 @@
-// Your implementation goes here
+class Stack {
+  #stack = [];
+  constructor(...params) {
+    this.#stack = params;
+  }
+
+  get size() {
+    return this.#stack.length;
+  }
+
+  push(element) {
+    this.#stack.push(element);
+  }
+
+  isEmpty() {
+    return this.#stack.length ? false : true;
+  }
+
+  pop() {
+    return this.#stack.length === 0 ? null : this.#stack.pop();
+  }
+
+  peek() {
+    return this.#stack[this.#stack.length - 1];
+  }
+
+  swap() {
+    const top = this.#stack[this.#stack.length - 1];
+    this.#stack[this.#stack.length - 1] = this.#stack[this.#stack.length - 2];
+    this.#stack[this.#stack.length - 2] = top;
+  }
+}
+
+module.exports = Stack;
